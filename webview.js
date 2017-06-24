4c6034eb1e3339d3b3fdd084bc847790bc5adbfc
Stefan Malzner <stefan@adlk.io>
2017-06-24 11:41:01 +0200

    Add visual indicator for unread threads and messages (#1)
    * set login as startpage
    
    * add notification count

--- webview.js
+++ webview.js
@@ -1,1 +1,9 @@
-module.exports = (Franz, options) => {}
+module.exports = (Franz) => {
+  const getMessages = function getMessages() {
+    const count = document.querySelectorAll('.switch_pane>.unread').length;
+
+    Franz.setBadge(count);
+  };
+
+  Franz.loop(getMessages);
+};
